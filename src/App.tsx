3e692262c6e9b689f913d5269b1904f8b9d881ef
RandomZiom <dkonik10@gmail.com>
2023-04-03 23:22:40 +0200

    Working, looking navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { useState } from 'react';
+import Navbar from './components/Navbar';
+
+// use state hook to change current page
+
 function App() {
-  return <div className="text-center selection:bg-green-900"></div>;
+  const [currentPage, setCurrentPage] = useState<JSX.Element>(
+    <div className="m-auto text-center">EMPTY PAGE</div>,
+  );
+
+  return (
+    <div className="text-center selection:bg-pink-100">
+      <Navbar onSubpageChange={(page) => setCurrentPage(page)}></Navbar>
+      <div>{currentPage}</div>
+    </div>
+  );
 }
 
 export default App;
